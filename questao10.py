c42b2a787393c98dd5958ea42c98ea315a614dcb
cleide <113257383+cidainfor@users.noreply.github.com>
2022-09-26 19:11:26 -0300

    Update questao10.py

--- questao10.py
+++ questao10.py
@@ -8,13 +8,11 @@ dia2 = int(input("digite o dia (1 a 31): "))
 mes2 = int(input("digite o mês (1 a 12): "))
 ano2 = int(input("digite o ano: "))
 
-#Comparativo de Anos
 if ano1 > ano2:
         print(f"a maior data é {dia1}/{mes1}/{ano1}")
 elif ano2 > ano1:
         print(f"a maior data é {dia2}/{mes2}/{ano2}")
 
-#Comparativo de Meses
 if mes1 > mes2:
     if ano1 > ano2:
         print(f"a maior data é {dia1}/{mes1}/{ano1}")
@@ -27,7 +25,6 @@ elif mes2 > mes1:
     elif ano2 > ano1:
         print(f"a maior data é {dia2}/{mes2}/{ano2}")
 
-#Comparativo de dias
 if dia1> dia2:
     if ano1 > ano2:
         print(f"a maior data é {dia1}/{mes1}/{ano1}")
