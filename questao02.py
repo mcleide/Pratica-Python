981f38b6fc9e36726e36de6b20ad746a5777f192
cleide <113257383+cidainfor@users.noreply.github.com>
2022-09-26 08:02:00 -0300

    Update questao02.py

--- questao02.py
+++ questao02.py
@@ -1,16 +1,15 @@
-nota1 = float(input("digite a primeira média: "))
-nota2 = float(input("digite a segunda média: "))
-nota3 = float(input("digite a terceria média: "))
+media1 = float(input("digite a primeira média: "))
+media2 = float(input("digite a segunda média: "))
+media3 = float(input("digite a terceria média: "))
 
-media = (nota1 + nota2 + nota3) / 3
-exame = 6 - media
+mediaA = (media1 + media2 + media3) / 3
+exame = 6 - mediaA
 
-if media < 3:
-    print("reprovado")
-elif media >= 3 and media < 6:
-    print(f"você precisa de {round(exame,2)} para ser aprovado")
-elif media >= 6:
-    print("aprovado")
-
-elif media >= 7:
-    print("aprovado")
+if mediaA < 3:
+       print("reprovado")
+    elif mediaA >= 3 and mediaA < 6:
+         print(f"você precisa de {round(exame,2)} para ser aprovado")
+        elif mediaA >= 6:
+             print("aprovado")
+            elif mediaA >= 7:
+                print("aprovado")
